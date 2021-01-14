1497f848e2b608351cd853efb68bd469a7c20d21
yyajima21 <yosuke.yajima21@gmail.com>
2021-01-14 11:46:35 -0500

    added a tensorboard feature.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repo is implementation for [PointNet](http://openaccess.thecvf.com/content_cvpr_2017/papers/Qi_PointNet_Deep_Learning_CVPR_2017_paper.pdf) and [PointNet++](http://papers.nips.cc/paper/7095-pointnet-deep-hierarchical-feature-learning-on-point-sets-in-a-metric-space.pdf) in pytorch.
 I modified the original code to have a custom training feature that has less than 13 classes. Note that this modification only applies to semantic scene segmentation model.
 ## Update
-**2020/8/23**
+**2021/1/14**
 The following inctruction contains how to run sementic segmentation on custom training dataset.
 ### Data Preparation
 Please download your custom dataset and create a folder called "data". Add your custom data to the data folder.
--- train_semseg.py
+++ train_semseg.py
@@ -16,6 +16,7 @@ from tqdm import tqdm
 import provider
 import numpy as np
 import time
+from torch.utils.tensorboard import SummaryWriter
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
@@ -33,7 +34,7 @@ for i,cat in enumerate(seg_classes.keys()):
 def parse_args():
     parser = argparse.ArgumentParser('Model')
     parser.add_argument('--model', type=str, default='pointnet_sem_seg', help='model name [default: pointnet_sem_seg]')
-    parser.add_argument('--batch_size', type=int, default=50, help='Batch Size during training [default: 16]')
+    parser.add_argument('--batch_size', type=int, default=25, help='Batch Size during training [default: 16]')
     parser.add_argument('--epoch',  default=128, type=int, help='Epoch to run [default: 128]')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='Initial learning rate [default: 0.001]')
     parser.add_argument('--gpu', type=str, default='0', help='GPU to use [default: GPU 0]')
@@ -44,7 +45,7 @@ def parse_args():
     parser.add_argument('--step_size', type=int,  default=10, help='Decay step for lr decay [default: every 10 epochs]')
     parser.add_argument('--lr_decay', type=float,  default=0.7, help='Decay rate for lr decay [default: 0.7]')
     parser.add_argument('--test_area', type=int, default=5, help='Which area to use for test, option: 1-6 [default: 5]')
-
+    parser.add_argument('--data', type=str, default='s3dis', help='mode [default: s3dis]')
     return parser.parse_args()
 
 def main(args):
@@ -149,6 +150,11 @@ def main(args):
     global_epoch = 0
     best_iou = 0
 
+    # Initialize a tensorboard class object
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    tb = SummaryWriter(comment=f"DATA_{args.data}_LR_{args.learning_rate}_BS_{args.batch_size}")
+    print("mode: {} device: {} lr: {} batch size: {}".format(args.data,device,args.learning_rate,args.batch_size))
+    
     for epoch in range(start_epoch,args.epoch):
         '''Train on chopped scenes'''
         log_string('**** Epoch %d (%d/%s) ****' % (global_epoch + 1, epoch + 1, args.epoch))
@@ -186,6 +192,12 @@ def main(args):
             total_correct += correct
             total_seen += (BATCH_SIZE * NUM_POINT)
             loss_sum += loss
+        log_string('Training mean loss: %f' % (loss_sum / num_batches))
+        log_string('Training accuracy: %f' % (total_correct / float(total_seen)))
+        # tensorboard log
+        tb.add_scalar("train loss", (loss_sum / num_batches), epoch)
+        tb.add_scalar("train accuracy", (total_correct / float(total_seen)), epoch)
+
         log_string('Training mean loss: %f' % (loss_sum / num_batches))
         log_string('Training accuracy: %f' % (total_correct / float(total_seen)))
 
@@ -266,7 +278,10 @@ def main(args):
                 torch.save(state, savepath)
                 log_string('Saving model....')
             log_string('Best mIoU: %f' % best_iou)
+        tb.add_scalar("valid loss", (loss_sum / num_batches), epoch)
+        tb.add_scalar("valid accuracy", (total_correct / float(total_seen)), epoch)
         global_epoch += 1
+    tb.close()
 
 
 if __name__ == '__main__':
