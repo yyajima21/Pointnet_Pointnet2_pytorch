1a7a65ef89162e3bd05c7c1be8127cafa45f1d7f
yyajima21 <yosuke.yajima21@gmail.com>
2021-01-14 14:05:16 -0500

    added pointnet_light model.

--- models/pointnet_sem_seg_light.py
+++ models/pointnet_sem_seg_light.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.utils.data
 import torch.nn.functional as F
-from pointnet import PointNetEncoder, feature_transform_reguliarzer
+from pointnet_light import PointNetEncoder, feature_transform_reguliarzer
 
 
 class get_model(nn.Module):
@@ -16,20 +16,20 @@ class get_model(nn.Module):
         self.k = num_class
         self.feat = PointNetEncoder(global_feat=False, feature_transform=True, channel=channel)
         self.conv1 = torch.nn.Conv1d(1088, 512, 1)
-        self.conv2 = torch.nn.Conv1d(512, 256, 1)
-        self.conv3 = torch.nn.Conv1d(256, 128, 1)
-        self.conv4 = torch.nn.Conv1d(128, self.k, 1)
+        #self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        #self.conv3 = torch.nn.Conv1d(256, 128, 1)
+        self.conv4 = torch.nn.Conv1d(512, self.k, 1)
         self.bn1 = nn.BatchNorm1d(512)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.bn3 = nn.BatchNorm1d(128)
+        #self.bn2 = nn.BatchNorm1d(256)
+        #self.bn3 = nn.BatchNorm1d(128)
 
     def forward(self, x):
         batchsize = x.size()[0]
         n_pts = x.size()[2]
         x, trans, trans_feat = self.feat(x)
         x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        #x = F.relu(self.bn2(self.conv2(x)))
+        #x = F.relu(self.bn3(self.conv3(x)))
         x = self.conv4(x)
         x = x.transpose(2,1).contiguous()
         x = F.log_softmax(x.view(-1,self.k), dim=-1)
